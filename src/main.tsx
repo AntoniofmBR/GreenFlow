1424a20c6b5ad11d041e1b6b51768e7789cf6edb
Antonio Fonseca <antonio.fonse06@gmail.com>
2024-08-25 14:44:25 -0300

    Fix error merge

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-<<<<<<< HEAD
 import { App } from './App'
-=======
-import App from './App'
->>>>>>> 848df59557ac563a6d88952ac89a161e6f0387ad
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
